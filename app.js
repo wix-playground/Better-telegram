f6e293f8b38dd72950169070699d2a16ef493174
oksanab <oksanab@wix.com>
2019-04-10 14:18:59 +0300

    added telegraf

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 
+const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4');
+app.use(bot.webhookCallback('/secret-path'));
+bot.telegram.setWebhook('https://https://bettertelegram.herokuapp.com/secret-path');
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('Hello World!')
 });
 
 app.listen(process.env.PORT || 3000);
