346f95f0698966cae71852c520a9fe7de8ceafa5
oksanab <oksanab@wix.com>
2019-04-10 14:34:56 +0300

    start

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4');
 app.use(bot.webhookCallback('/secret-path'));
 bot.telegram.setWebhook('https://https://bettertelegram.herokuapp.com/secret-path');
 
+bot.start((ctx, next) => {
+    next();
+}, (ctx) => {
+    ctx.reply('Magical World')
+});
+
+// bot.command('cast', () => {
+//
+// });
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+
 
 app.listen(process.env.PORT || 3000);
