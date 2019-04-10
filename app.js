ebdbb9d7f1b1ade15e40a7e5bb56627d843c4677
oksanab <oksanab@wix.com>
2019-04-10 16:48:09 +0300

    loggin

--- app.js
+++ app.js
@@ -1,62 +1,18 @@
-// const Telegraf = require('telegraf');
-//
-// const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4');
-//
-// // bot.telegram.deleteWebhook().then(() => {
-// //     bot.startPolling();
-// //     bot.start((ctx) => ctx.reply('Welcome'));
-// //     bot.help((ctx) => ctx.reply('Send me a sticker'));
-// //     bot.command('createBet', (ctx) => {
-// //         console.log('AGRUMENTS', ...arguments);
-// //     })
-// //     bot.on('sticker', (ctx) => ctx.reply('👍'));
-// //     bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-// // });
-// //
-//
-// const { Composer } = Telegraf
-//
-//
-// bot.on('poll', (ctx) => ctx.reply('Poll update'))
-//
-// bot.start((ctx) => ctx.telegram.sendPoll('-100500100500100', '2b|!2b', ['True', 'False']))
-//
-// bot.command('poll', Composer.groupChat(
-//     (ctx) => ctx.replyWithPoll('2b|!2b', ['True', 'False'])
-// ))
-//
-// bot.launch();
+const Telegraf = require('telegraf');
 
-const Telegraf = require('telegraf')
-const fetch = require('node-fetch')
+const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4');
 
-// async/await example.
+bot.telegram.deleteWebhook().then(() => {
+    bot.start((ctx) => ctx.reply('Welcome'));
+    bot.help((ctx) => ctx.reply('Send me a sticker'));
+    bot.command('createBet', (ctx) => {
+        console.log('AGRUMENTS', ...arguments);
+        console.log('CTX', ctx);
+    });
+    bot.on('sticker', (ctx) => ctx.reply('👍'));
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+    bot.startPolling();
+});
 
-async function omdbSearch (query = '') {
-    const apiUrl = `http://www.omdbapi.com/?s=${query}&apikey=9699cca`
-    const response = await fetch(apiUrl)
-    console.log('response', response);
-    const json = await response.json()
-    const posters = (json.Search && json.Search) || [];
-    console.log('posters', posters);
-    return posters.filter(({ Poster }) => Poster && Poster.startsWith('https://')) || []
-}
+bot.launch();
 
-const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4')
-bot.use(Telegraf.log());
-bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
-    console.log('inlineQuery',inlineQuery);
-    console.log('answerInlineQuery',answerInlineQuery);
-    const posters = await omdbSearch(inlineQuery.query)
-    const results = posters.map((poster) => ({
-        type: 'photo',
-        id: poster.imdbID,
-        caption: poster.Title,
-        description: poster.Title,
-        thumb_url: poster.Poster,
-        photo_url: poster.Poster
-    }))
-    return answerInlineQuery(results)
-})
-
-bot.launch()
