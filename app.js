0391e58f52d1f02559cbd873d610c201702d8018
oksanab <oksanab@wix.com>
2019-04-10 15:48:11 +0300

    fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4', {
     username: 'threeaxe_bot'
 });
 
-bot.deleteWebhook().then(() => {
+bot.telegram.deleteWebhook().then(() => {
     // bot.startPolling();
     bot.start((ctx) => ctx.reply('Welcome'));
     bot.help((ctx) => ctx.reply('Send me a sticker'));
