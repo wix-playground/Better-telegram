0a86a4ff817f6f26da2511b0a2270ff8d978b103
oksanab <oksanab@wix.com>
2019-04-10 14:41:14 +0300

    log

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4');
+const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4', {
+    channelMode: true
+});
 app.use(bot.webhookCallback('/secret-path'));
-bot.telegram.setWebhook('https://https://bettertelegram.herokuapp.com/secret-path');
+bot.telegram.deleteWebhook().then(() => {
+    bot.telegram.setWebhook('https://https://bettertelegram.herokuapp.com/secret-path');
+});
 
 bot.start((ctx, next) => {
     console.log('within middleware');
