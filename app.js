3c6ba13c1eb70d5082c30d468803770a9fd9ace9
iaroslavs <iaroslavs@wix.com>
2019-04-10 17:05:15 +0300

    session

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const Telegraf = require('telegraf');
+const LocalSession = require('telegraf-session-local');
 
 const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4');
+
+bot.use((new LocalSession({ database: 'example_db.json' })).middleware());
 
 bot.telegram.deleteWebhook().then(() => {
     bot.start((ctx) => ctx.reply('Welcome'));
     bot.help((ctx) => ctx.reply('Send me a sticker'));
     bot.command('createBet', (ctx) => {
-        console.log('AGRUMENTS', ...arguments);
-        console.log('CTX', ctx);
+        const payload = ctx.text.replace('/createBet ', '');
+        const [id, outcome] = payload.split(' ');
+        ctx.session[id] = {
+            outcome,
+        };
+    });
+    bot.command('listBets', (ctx) => {
+        return ctx.reply(JSON.stringify(ctx.session));
     });
     bot.on('sticker', (ctx) => ctx.reply('👍'));
     bot.hears('hi', (ctx) => ctx.reply('Hey there'));
