df66da649c05a91dedb0596386b6263371632443
iaroslavs <iaroslavs@wix.com>
2019-04-10 15:43:35 +0300

    fix

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+
 const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4', {
     channelMode: true,
     username: 'threeaxe_bot'
 });
 
-bot.deleteWebhook();
-bot.startPolling();
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+bot.deleteWebhook().then(() => {
+    bot.startPolling();
+    bot.start((ctx) => ctx.reply('Welcome'));
+    bot.help((ctx) => ctx.reply('Send me a sticker'));
+    bot.on('sticker', (ctx) => ctx.reply('👍'));
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+});
+
