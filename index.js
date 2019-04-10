d6201780dfb3917ef19524be899d568ced1172df
oksanab <oksanab@wix.com>
2019-04-10 13:55:47 +0300

    pure node and express

--- index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const bootstrap = require('@wix/wix-bootstrap-ng');
-
-const src = process.env.SRC_PATH || path.join('.', 'dist', 'src');
-const server = path.join(src, 'server');
-
-bootstrap()
-  .express(server)
-  .start({ disableCluster: process.env.NODE_ENV === 'development' });
