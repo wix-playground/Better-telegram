6d7f9a611a46b269728653005ed67457f34d34ec
oksanab <oksanab@wix.com>
2019-04-10 15:35:45 +0300

    polling

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Telegraf = require('telegraf')
 const bot = new Telegraf('815343171:AAE2jekFZx4xSF0XJMcIymXFxqvkjV8ecM4', {
-    channelMode: true
+    channelMode: true,
+    username: 'threeaxe_bot'
 });
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+bot.startPolling();
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
